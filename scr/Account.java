27e4e89b6a5452ba5d4b87d310eb12d38ff20dc7
sergiistepanenko <sergiystep@gmail.com>
2024-02-05 22:46:37 +0200

    First Commit

---
+++ scr/Account.java
@@ -0,0 +1,11 @@
+public class Account {
+    public String password;
+
+    public Account(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
